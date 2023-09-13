cdabca2eb741ec01e3cd5bdd71262d50b0289d17
danh-ld <danh-ld@genkisystem.com>
2023-09-13 17:25:43 +0700

    test script

--- flutter_drive_fail_on_overflow.sh
+++ flutter_drive_fail_on_overflow.sh
@@ -1,10 +1,50 @@
+# #!/bin/bash
+
+# # Thực thi Flutter Driver và kiểm tra logs ngay lập tức
+# error=$(flutter drive --target=test_driver/app.dart | tee test_logs.log | awk '
+#     /flutter: \[FlutterError\]/ { print; flag=1; next }
+#     flag && !/<…>/ { print }
+#     flag && /<…>/ { print; flag=0; exit 1 }
+# ')
+
+# # Kiểm tra exit code của awk
+# if [ $? -eq 1 ]; then
+#     echo "Error detected:"
+#     echo "$error"
+#     exit 1
+# fi
+
+###########################################
 #!/bin/bash
 
-# Thực thi Flutter Driver và lưu logs
-flutter drive --target=test_driver/app.dart | tee test_logs.log | awk '/EXCEPTION CAUGHT BY RENDERING LIBRARY/ { exit 1 }'
+# Tạo named pipe
+mkfifo pipe
 
-# Kiểm tra xem log có chứa "EXCEPTION CAUGHT BY RENDERING LIBRARY" hay không
-if grep -q "EXCEPTION CAUGHT BY RENDERING LIBRARY" test_logs.log; then
-    echo "Error detected!"
+# Chạy flutter driver và đưa output vào named pipe
+flutter drive --target=test_driver/app.dart > pipe &
+
+# Lưu pid của flutter driver
+flutter_pid=$!
+
+# Đặt trap để dừng flutter driver khi script kết thúc
+trap "kill $flutter_pid 2> /dev/null" EXIT
+
+# Đọc từ named pipe với awk
+error=$(awk '
+    /flutter: \[FlutterError\]/ { print; flag=1; next }
+    flag && !/<…>/ { print }
+    flag && /<…>/ { print; flag=0; exit 1 }
+' < pipe)
+
+# Kiểm tra exit code của awk
+awk_exit_code=$?
+
+# Nếu awk đã thoát với mã 1 (phát hiện lỗi), in lỗi và dừng script
+if [ $awk_exit_code -eq 1 ]; then
+    echo "Error detected:"
+    echo "$error"
     exit 1
 fi
+
+# Xóa named pipe
+rm -rf pipe
