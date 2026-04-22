e5cece19123432ba860160e52517ffacf988de87
danh-ld <danh-ld@genkisystem.com>
2026-04-22 11:57:17 +0700

    style: update text label from "Add more Todo" to "Add another Todo"

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
             height: 20,
           ),
           const Text(
-            "Add more Todo",
+            "Add another Todo",
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
